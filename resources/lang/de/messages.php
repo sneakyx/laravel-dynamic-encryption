25f8778e03fa413e31f1b7041b957f5eba8dd6b0
André Scholz <andre.scholz@goodjobs.eu>
2026-01-06 15:32:31 +0100

    pinting

--- resources/lang/de/messages.php
+++ resources/lang/de/messages.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     'saving_failed_key_missing' => 'Speichern nicht möglich: Der dynamische Verschlüsselungsschlüssel ist nicht verfügbar. Bitte kontaktieren Sie einen Administrator.',
 ];
--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     'saving_failed_key_missing' => 'Saving not possible: Dynamic encryption key is not available. Please contact an administrator.',
 ];
